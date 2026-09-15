df6914a96843b7fde89ff6c5e2132bfe1669bbe2
paulodirectresponse-jpg <paulo.direct.response@gmail.com>
2026-09-15 20:13:11 -0300

    perf: initialize browser performance observers

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,11 @@
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
+import {installPerformanceObservers} from './utils/performanceMetrics.js';
 import './index.css';
 import './styles/enterprise-theme.css';
+
+installPerformanceObservers();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
