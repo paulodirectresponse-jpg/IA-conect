91ba827f41d0ee0370bb8ba87f4888705b469afe
paulodirectresponse-jpg <paulo.direct.response@gmail.com>
2026-09-16 14:04:02 -0300

    beta: mount Task Center in global Beta shell

--- src/beta/BetaApp.tsx
+++ src/beta/BetaApp.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { ArrowLeft, FlaskConical } from 'lucide-react';
 import { BrandMark } from '../components/common/BrandMark.js';
 import { BetaHomeView } from './views/BetaHomeView.js';
+import { TaskCenter } from './components/TaskCenter.js';
 import './styles/beta.css';
 
 interface BetaAppProps {
@@ -15,10 +16,13 @@ export const BetaApp: React.FC<BetaAppProps> = ({ onExit }) => (
         <BrandMark compact />
         <span className="ia-beta-badge"><FlaskConical aria-hidden="true" /> Beta</span>
       </div>
-      <button type="button" className="ia-beta-exit" onClick={onExit}>
-        <ArrowLeft aria-hidden="true" />
-        <span>Voltar à versão atual</span>
-      </button>
+      <div className="ia-beta-navbar-actions">
+        <TaskCenter />
+        <button type="button" className="ia-beta-exit" onClick={onExit}>
+          <ArrowLeft aria-hidden="true" />
+          <span>Voltar à versão atual</span>
+        </button>
+      </div>
     </header>
     <BetaHomeView />
   </div>
