9b476b494dde05fb1237bd40d445ae7b319f070b
paulodirectresponse-jpg <paulo.direct.response@gmail.com>
2026-09-16 17:54:43 -0300

    beta: add provider-independent Image Editor client

---
+++ src/beta/imageEditorClient.ts
@@ -0,0 +1,9 @@
+import { apiRequest } from '../services/apiClient.js';
+import { BetaCapabilityModel } from './capabilityClient.js';
+
+export const betaImageEditorClient={
+  async catalog(){
+    const data=await apiRequest<{models:BetaCapabilityModel[]}>('/api/beta/capabilities');
+    return(data.models||[]).filter(model=>model.category==='IMAGE'&&model.capabilities.some(cap=>['image-edit','inpaint-mask','background-remove-replace','outpaint','upscale','variations'].includes(cap.id)));
+  },
+};
